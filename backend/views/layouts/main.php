229f30d2954e417660c7834c9049e5e8238212e2
Robert Beckson <becksonq@gmail.com>
2017-10-25 21:40:21 +0300

    'yii2-user-finish'

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= Html::encode(Yii::$app->name) ?></title>
     <?php $this->head() ?>
 </head>
 <body>
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -68,7 +68,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= Yii::$app->name ?> <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'vendorPath' => dirname( dirname( __DIR__ ) ) . '/vendor',
+    'name' => 'Vezugruz29.ru',
     'language' => 'ru-RU',
     'sourceLanguage' => 'ru-RU',
     'components' => [
